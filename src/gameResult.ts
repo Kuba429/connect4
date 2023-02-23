1bbb89ee186c922a574451802056a93db7d11f5f
Kuba429 <jakubzawadzki429@gmail.com>
2023-02-23 21:26:08 +0100

    win condition - vertical

--- src/gameResult.ts
+++ src/gameResult.ts
@@ -23,6 +23,8 @@ export const checkResultByCell = (
 ): winResult | null => {
 	const hor = checkHorizontal(x, y, board);
 	if (hor) return hor;
+	const ver = checkVertical(x, y, board);
+	if (ver) return ver;
 	return null;
 };
 
@@ -54,3 +56,27 @@ export const checkHorizontal = (
 	return null;
 };
 
+export const checkVertical = (
+	x: number,
+	y: number,
+	board: cell[][]
+): winResult | null => {
+	// horizontal line
+	const og = board[y][x].value;
+	if (!og) return null;
+	const line = [board[y][x]];
+	let lowest = y;
+	let highest = y;
+	// go down until the values are the same
+	while (highest + 1 < board.length && board[highest + 1][x].value === og) {
+		line.push(board[highest + 1][x]);
+		highest++;
+	}
+	// then go up
+	while (lowest - 1 >= 0 && board[lowest - 1][x].value === og) {
+		line.unshift(board[lowest - 1][x]);
+		lowest--;
+	}
+	if (line.length >= 4) return { cells: line, winner: og };
+	return null;
+};
--- tests/gameResult.test.ts
+++ tests/gameResult.test.ts
@@ -24,3 +24,27 @@ test("Winning condition - horizontal", () => {
 	result = checkResultByCell(0, 2, board);
 	expect(result).toBeNull();
 });
+
+test("Winning condition - vertical", () => {
+	let board = getEmptyBoard();
+	let result = checkResult(board);
+	// empty board
+	expect(result).toBeNull();
+	// [x,y]
+	[
+		[4, 1],
+		[4, 2],
+		[4, 3],
+		[4, 4],
+	].forEach(([x, y]) => (board[y][x].value = 1));
+	// board with winning line made of ones
+	result = checkResult(board);
+	expect(result).not.toBeNull();
+	expect(result?.winner).toBe(1);
+	result = checkResultByCell(4, 3, board);
+	expect(result?.winner).toBe(1);
+	// change the board so 1 is not winning anymore
+	board[2][4].value = 2; // one of the cells to which 1 was assigned before
+	result = checkResultByCell(4, 3, board);
+	expect(result).toBeNull();
+});
