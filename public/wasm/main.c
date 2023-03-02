bcfda6a484d14eb60ddf275f6ef01f995023d0a1
Kuba429 <jakubzawadzki429@gmail.com>
2023-03-02 21:03:34 +0100

    reshape array, fix reference passing

--- public/wasm/main.c
+++ public/wasm/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct position {
   int x;
@@ -16,31 +17,41 @@ int toggle_turn();
 struct position insert_cell();
 
 int get_best_move(int8_t board_prop[6 * 7], int turn, int player, int ai) {
+  // reshape the array to 2 dimensions
   int board[6][7] = {0};
-  // TODO reshape the board array
+  int i = 0;
+  for (int y = 0; y < 6; y++) {
+    for (int x = 0; x < 7; x++) {
+      board[y][x] = board_prop[i++];
+    }
+  }
   int best_x = 0;
   int best_score = -10000;
-  //  for (int i = 0; i < 7; i++) {
-  //    struct position cell = insert_cell(&board, i, turn);
-  //    if (cell.x == -1 || cell.y == -1)
-  //      continue;
-  //    int current_score = minimax(&board, cell, turn, player, ai, 0, 3);
-  //    board[cell.y][cell.x] = 0;
-  //    if (current_score > best_score) {
-  //      best_score = current_score;
-  //      best_x = i;
-  //    }
-  //  };
+  for (int i = 0; i < 7; i++) {
+    struct position cell = insert_cell(board, i, turn);
+    if (cell.x == -1 || cell.y == -1)
+      continue;
+    int current_score = minimax(board, cell, turn, player, ai, 0, 3);
+    board[cell.y][cell.x] = 0;
+    if (current_score > best_score) {
+      best_score = current_score;
+      best_x = i;
+    }
+  };
   return best_x;
 }
-int minimax(int *board[6][7], struct position last_cell, int turn, int player,
+int minimax(int board[6][7], struct position last_cell, int turn, int player,
             int ai, int maximizing, int depth) {
   int res = check_result_by_cell(last_cell.x, last_cell.y, board);
   if (res != 0) {
-    if (res == ai)
+    if (res == ai) {
+      printf("win");
       return (depth + 1) * 1;
-    if (res == player)
+    }
+    if (res == player) {
+      printf("lose");
       return (depth + 1) * -1;
+    }
   }
   if (depth < 1)
     return 0;
@@ -55,7 +66,7 @@ int minimax(int *board[6][7], struct position last_cell, int turn, int player,
       if (new_score >= best_score) {
         best_score = new_score;
       }
-      *board[cell.y][cell.x] = 0;
+      board[cell.y][cell.x] = 0;
     }
     return best_score;
   } else {
@@ -69,22 +80,21 @@ int minimax(int *board[6][7], struct position last_cell, int turn, int player,
       if (new_score <= best_score) {
         best_score = new_score;
       }
-      *board[cell.y][cell.x] = 0;
+      board[cell.y][cell.x] = 0;
     }
     return best_score;
   }
   return 0;
 }
-struct position insert_cell(int *board[6][7], int x, int y, int value) {
-  struct position cell = {-1, -1};
-  for (int y = 6; y >= 0; y--) {
+struct position insert_cell(int board[6][7], int x, int y, int value) {
+  for (int y = 5; y >= 0; y--) {
     if (board[y][x] == 0) {
-      *board[y][x] = value;
-      cell.x = x;
-      cell.y = y;
-      break;
+      board[y][x] = value;
+      struct position cell = {x, y};
+      return cell;
     }
   }
+  struct position cell = {-1, -1};
   return cell;
 }
 // true to false and vice versa
