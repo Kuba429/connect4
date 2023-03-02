cc8098c9abc856b1d6d55b60d687d42e83e8c88d
Kuba429 <jakubzawadzki429@gmail.com>
2023-03-02 16:12:15 +0100

    more porting

--- public/wasm/main.c
+++ public/wasm/main.c
@@ -13,12 +13,77 @@ int minimax();
 int check_result_by_cell();
 int toggle_maximizing();
 int toggle_turn();
+struct position insert_cell();
 
-int get_best_move(int board[6][7], int turn, int player, int ai) { return 0; }
-int minimax(int board[6][7], struct position last_cell, int turn,
-             int player, int ai, int maximizing, int depth) {
+int get_best_move(int board[6][7], int turn, int player, int ai) {
+  int best_x = 0;
+  int best_score = -10000;
+  for (int i = 0; i < 7; i++) {
+    struct position cell = insert_cell(&board, i, turn);
+    if (cell.x == -1 || cell.y == -1)
+      continue;
+    int current_score = minimax(&board, cell, turn, player, ai, 0, 3);
+    board[cell.y][cell.x] = 0;
+    if (current_score > best_score) {
+      best_score = current_score;
+      best_x = i;
+    }
+  };
+  return best_x;
+}
+int minimax(int *board[6][7], struct position last_cell, int turn, int player,
+            int ai, int maximizing, int depth) {
   int res = check_result_by_cell(last_cell.x, last_cell.y, board);
+  if (res != 0) {
+    if (res == ai)
+      return (depth + 1) * 1;
+    if (res == player)
+      return (depth + 1) * -1;
+  }
+  if (depth < 1)
+    return 0;
+  if (maximizing) {
+    int best_score = -10000;
+    for (int i = 0; i < 7; i++) {
+      struct position cell = insert_cell(board, i, ai);
+      if (cell.x == -1 || cell.y == -1)
+        continue;
+      int new_score = minimax(board, cell, toggle_turn(turn), player, ai,
+                              toggle_maximizing(maximizing), depth - 1);
+      if (new_score >= best_score) {
+        best_score = new_score;
+      }
+      *board[cell.y][cell.x] = 0;
+    }
+    return best_score;
+  } else {
+    int best_score = 10000;
+    for (int i = 0; i < 7; i++) {
+      struct position cell = insert_cell(board, i, player);
+      if (cell.x == -1 || cell.y == -1)
+        continue;
+      int new_score = minimax(board, cell, toggle_turn(turn), player, ai,
+                              toggle_maximizing(maximizing), depth - 1);
+      if (new_score <= best_score) {
+        best_score = new_score;
+      }
+      *board[cell.y][cell.x] = 0;
+    }
+    return best_score;
+  }
   return 0;
+}
+struct position insert_cell(int *board[6][7], int x, int y, int value) {
+  struct position cell = {-1, -1};
+  for (int y = 6; y >= 0; y--) {
+    if (board[y][x] == 0) {
+      *board[y][x] = value;
+      cell.x = x;
+      cell.y = y;
+      break;
+    }
+  }
+  return cell;
 }
 // true to false and vice versa
 int toggle_maximizing(int bool) {
